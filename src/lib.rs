ebd90ea9c56b35c022a3186ec51c9c099801bf60
pussbb <pussbb@gmail.com>
2024-09-12 17:45:25 +0200

    libolm basics import

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,26 @@
 #![deny(clippy::all)]
 
-#[macro_use]
-extern crate napi_derive;
+use napi::bindgen_prelude::*;
+use napi_derive::napi;
+
+#[napi]
+pub fn throw_error() -> Result<()> {
+    Err(Error::new(Status::GenericFailure, "Manual Error".to_owned()))
+}
+
+#[napi]
+pub struct OlmMessage {
+    pub ciphertext: String,
+    pub message_type: u32,
+}
+
+#[napi]
+impl OlmMessage {
+    #[napi(constructor)]
+    pub fn new(message_type: u32, ciphertext: String) -> Self {
+        Self {
+            ciphertext,
+            message_type,
+        }
+    }
+}
