a1a29b5a2d47e620e43e8f96e5e321026a5d14d4
pussbb <pussbb@gmail.com>
2024-09-18 19:38:37 +0200

    optional dependecies

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,7 +1,8 @@
 import test from 'ava'
 
 import {Account, SessionConfig} from '../index.js'
-import {isObject, isString} from "lodash-es";
+import {isEqual, isObject, isString} from "lodash-es";
+import {equal} from "node:assert";
 
 test('Account init from native', (t) => {
   const acc = new Account()
@@ -18,10 +19,13 @@ test('Account generate keys', (t) => {
 
 test('Account pickle keys', (t) => {
   const acc = new Account()
+  acc.generateOneTimeKeys(50)
   t.throws(() => acc.pickle('sddd'))
   const pass = '21gCTR9zmeMq6hpH7DlAUeXIMWMBrAUe'
   t.true(isString(acc.pickle(pass)))
-  t.true(isObject(Account.fromPickle(acc.pickle(pass), pass)))
+  const acc2 = Account.fromPickle(acc.pickle(pass), pass)
+  t.true(isObject(acc2))
+  t.true(Object.values(acc2.oneTimeKeys).length > 0)
 })
 
 
@@ -35,15 +39,22 @@ test('Account sessions', (t) => {
   const alice = new Account();
   const bob = new Account;
 
-  bob.generateOneTimeKeys(1);
+  bob.generateOneTimeKeys(4);
 
-  const session = alice.createOutboundSession(bob.curve25519Key, Object.values(bob.oneTimeKeys)[0], SessionConfig.version2());
+  const bobOnetimeKeys =  Object.values(bob.oneTimeKeys)
+  const bobFirstOnetimeKey = bobOnetimeKeys[0]
+
+  const session = alice.createOutboundSession(bob.curve25519Key, bobFirstOnetimeKey, SessionConfig.version2());
   const res = session.encrypt('Hello there')
   let { plaintext: decrypted, session: bob_session } = bob.createInboundSession(alice.curve25519Key, res);
   t.true(decrypted  === 'Hello there')
   const message = bob_session.encrypt('ddddd');
   const decrypted2 = session.decrypt(message);
   t.true(decrypted2 === 'ddddd')
+
+  // one time key removes on first usage
+  t.false(isEqual(bobOnetimeKeys, Object.values(bob.oneTimeKeys)))
+  t.false(bobOnetimeKeys.includes(bobFirstOnetimeKey))
   ///t.true(isString(res))
    //       bob.curve25519_key(),
    //   *bob.one_time_keys().values().next().unwrap(),
